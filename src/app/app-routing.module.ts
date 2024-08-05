12ab7d58e85fead9acefee6e98a7190e70fca642
karoolina1234 <karoolina.sm@gmail.com>
2024-08-05 16:24:34 -0300

    Criando o componente dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './modules/home/home.component';
+import { DashboardHomeComponent } from './modules/dashboard/page/dashboard-home/dashboard-home.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
+  },
+  {
+    path: 'dashboard',
+    component: DashboardHomeComponent,
   },
 ];
 
