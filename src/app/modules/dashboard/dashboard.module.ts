31d021b216c03e978c554acb5033cc1c15a7bedc
karoolina1234 <karoolina.sm@gmail.com>
2024-08-06 16:27:36 -0300

    Iniciando a implementação do componente de toolbar

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -14,6 +14,7 @@ import { MessageService } from 'primeng/api';
 import { CookieService } from 'ngx-cookie-service';
 
 import { ChartModule } from 'primeng/chart';
+import { SharedModule } from 'src/app/shared/shared.module';
 @NgModule({
   declarations: [DashboardHomeComponent],
   imports: [
@@ -27,6 +28,7 @@ import { ChartModule } from 'primeng/chart';
     CardModule,
     ToastModule,
     ChartModule,
+    SharedModule
   ],
   providers: [MessageService, CookieService],
 })
