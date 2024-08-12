3fcbf0d09efb49b7d00f6fabc633e094b4923901
karoolina1234 <karoolina.sm@gmail.com>
2024-08-12 17:26:39 -0300

    Usando o output

--- src/app/models/enums/products/productEvent.ts
+++ src/app/models/enums/products/productEvent.ts
@@ -1,4 +1,4 @@
-export enum PorductEvent {
+export enum ProductEvent {
   ADD_PRODUCT_EVENT = 'Adicionar produto',
   EDIT_PRODUCT = 'Editar produto',
   SALE_PRODUCT_EVENT = 'Vender produto',
--- src/app/modules/products/components/products-table/products-table.component.ts
+++ src/app/modules/products/components/products-table/products-table.component.ts
@@ -1,4 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { ProductEvent } from 'src/app/models/enums/products/productEvent';
+import { EventAction } from 'src/app/models/interfaces/products/event/EventAction';
 import { GetAllProductsResponse } from 'src/app/models/interfaces/products/getAllProducts';
 
 @Component({
@@ -8,6 +10,19 @@ import { GetAllProductsResponse } from 'src/app/models/interfaces/products/getAl
 })
 export class ProductsTableComponent {
   @Input() products: Array<GetAllProductsResponse> = [];
+  @Output() productEvent = new EventEmitter<EventAction>();
 
   public productSelected!: GetAllProductsResponse;
+  public addProductEvent = ProductEvent.ADD_PRODUCT_EVENT;
+  public editProductEvent = ProductEvent.EDIT_PRODUCT;
+
+  handleProductEvent(action: string, id?: string): void {
+    if (action && action !== '') {
+      const productEventData = id && id !== '' ? { action, id } : { action };
+
+      //emitir evento
+
+      this.productEvent.emit(productEventData);
+    }
+  }
 }
